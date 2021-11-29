30653f93d45bb10fb0358d1020c8515aed3e566c
olegsokolansky <oleg.sokolansky@gmail.com>
2021-11-29 14:13:46 +0100

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,9 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
-  render(<App />);
-  const messageElement = screen.getByText(/Hello World/i);
-  expect(messageElement).toBeInTheDocument();
+  const { container } = render(<App />);
+  expect(container.getElementsByClassName("message-list").length).toBe(1);
+  expect(container.getElementsByClassName("new-message-wrapper").length).toBe(
+    1
+  );
 });
