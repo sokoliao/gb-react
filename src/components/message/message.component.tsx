5172b2f0ea45aa485040f551070a2cda7ffdb921
olegsokolansky <oleg.sokolansky@gmail.com>
2021-12-03 19:05:32 +0100

    wip3

--- src/components/message/message.component.tsx
+++ src/components/message/message.component.tsx
@@ -1,6 +1,7 @@
 import { Message } from "../../model/message";
 import "./message.css";
 import moment from "moment";
+import { Nav, Image, Col } from "react-bootstrap";
 
 interface MessageProps {
   message: Message;
@@ -10,15 +11,15 @@ interface MessageProps {
 export const MessageComponent: React.FC<MessageProps> = (props) => {
   return (
     <div
-      className={
-        props.isCurrentUserAuthor
-          ? "message-wrapper-reverse"
-          : "message-wrapper"
-      }
+      className={`d-flex align-items-end ${
+        props.isCurrentUserAuthor && "flex-row-reverse"
+      }`}
     >
-      <div className="message-logo">
-        <img src={props.message.user.logoUrl} alt="logo" />
-      </div>
+      <Image
+        className="message-logo m-2"
+        src={props.message.user.logoUrl}
+        alt="logo"
+      ></Image>
       <div key={props.message.id} className="message-text">
         <div className="message-user-name">{props.message.user.name}</div>
         <div>{props.message.text}</div>
@@ -28,4 +29,24 @@ export const MessageComponent: React.FC<MessageProps> = (props) => {
       </div>
     </div>
   );
+  // return (
+  //   <div
+  //     className={
+  //       props.isCurrentUserAuthor
+  //         ? "message-wrapper-reverse"
+  //         : "message-wrapper"
+  //     }
+  //   >
+  //     <div className="message-logo">
+  //       <img src={props.message.user.logoUrl} alt="logo" />
+  //     </div>
+  //     <div key={props.message.id} className="message-text">
+  //       <div className="message-user-name">{props.message.user.name}</div>
+  //       <div>{props.message.text}</div>
+  //       <div className="message-timestamp">
+  //         {moment(props.message.timestamp).format("h:mm")}
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 };
