f3f2b6b0bf72d14e8a51ea7115a6c0c6b8a7d2e1
olegsokolansky <oleg.sokolansky@gmail.com>
2021-12-08 15:40:10 +0200

    Update chats-layout.component.tsx

--- src/components/chats-layout/chats-layout.component.tsx
+++ src/components/chats-layout/chats-layout.component.tsx
@@ -1,17 +1,21 @@
+import _ from "lodash";
 import React, { useContext } from "react";
 import { Col, Row } from "react-bootstrap";
-import { Outlet } from "react-router";
+import { Outlet, useParams } from "react-router";
 import { AppStateContext } from "../../App";
 import { ChatListComponent } from "../chat-list/chat-list.component";
 
 export const ChatsLayoutComponent: React.FC<{}> = () => {
   const { chats } = useContext(AppStateContext);
+  const chatId = useParams()["chatId"];
+  const chat = _.find(chats, (c) => c.id === chatId);
   return (
     <Row className="overflow-hidden flex-grow-1 m-0 flex-nowrap">
       <Col xs="3" className="border-end">
         <ChatListComponent chats={chats}></ChatListComponent>
       </Col>
-      {<Outlet />}
+      {chat && <Outlet />}
+      {!chat && <h1>404</h1>}
     </Row>
   );
 };
